9cbbbb2c0b7aee3620eac58af7d9643d74fc42f1
pawan575 <chauhanpawan062@gmail.com>
2026-08-01 15:31:55 +0530

    Time: 1 ms (63.47%), Space: 43.2 MB (55.09%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,19 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : stones.toCharArray())
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        int ans = 0;
-        for (int i = 0; i < jewels.length(); i++) {
-            char ch = jewels.charAt(i);
-            if (map.containsKey(ch)) {
-                ans += map.get(ch);
-            }
-        }
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for (char ch : stones.toCharArray())
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // int ans = 0;
+        // for (int i = 0; i < jewels.length(); i++) {
+        //     char ch = jewels.charAt(i);
+        //     if (map.containsKey(ch)) {
+        //         ans += map.get(ch);
+        //     }
+        // }
+        int[] freq=new int[200];
+        for(char ch: stones.toCharArray()) freq[ch-'A']++;
+        int ans=0;
+        for(char ch:jewels.toCharArray()) ans+=freq[ch-'A'];
         return ans;
     }
 }
